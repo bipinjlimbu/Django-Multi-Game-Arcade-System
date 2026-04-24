4d51bd8de237bb970e5974ca5bd468a8840cf34f
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-24 08:08:38 +0545

    Leaderboard Model Created

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Leaderboard(models.Model):
+    class choices(models.TextChoices):
+        GUESS_GAME = 'Guess Game'
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    game = models.CharField(max_length=50, choices=choices.choices)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.game}: {self.score}"
