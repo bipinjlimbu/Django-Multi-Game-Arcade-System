5c1e40b22707d80fc7f502ad665367ee3212f103
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-24 08:03:30 +0545

    Guess Game Data Fetched in Backend

--- my_app/views/game_view.py
+++ my_app/views/game_view.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render, redirect
+import json
 
 def guess_view(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            name = data.get('game_name')
+            score = data.get('score')
+            print(f"Received score for {name}: {score}")
+        except json.JSONDecodeError:
+            print("Invalid JSON received")
+            return redirect('guess')
+        return redirect('guess')
     return render(request, 'games/guess_game.html')
