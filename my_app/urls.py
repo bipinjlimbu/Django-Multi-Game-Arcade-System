189844adb67d3ace5abc25d071f2cbec46e65432
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-24 08:24:15 +0545

    Leaderboard Page Setup Done

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
-from .views.main import home_view, games_view
+from .views.main import home_view, games_view, leaderboard_view
 from .views.game_view import guess_view
 
 urlpatterns = [
@@ -9,5 +9,6 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('games/', games_view, name='games'),
+    path('leaderboard/<str:game_name>/', leaderboard_view, name='leaderboard'),
     path('guess/', guess_view, name='guess'),
 ]
--- my_app/views/main.py
+++ my_app/views/main.py
@@ -5,3 +5,7 @@ def home_view(request):
 
 def games_view(request):
     return render(request, 'main/games_page.html')
+
+def leaderboard_view(request, game_name):
+    return render(request, 'main/leaderboard_page.html', {'game_name': game_name})
+    
