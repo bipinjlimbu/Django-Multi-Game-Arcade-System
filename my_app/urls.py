ad3713acc2f8da3741192f7636d1541f0f6f0fce
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-24 06:46:49 +0545

    Register Page Setup Done

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views.auth_view import register_view
 from .views.main import home_view, games_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('register/', register_view, name='register'),
     path('games/', games_view, name='games'),
 ]
