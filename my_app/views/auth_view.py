5cb6ab30c1637c332f70a521fee4f267781c1458
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-24 07:14:23 +0545

    Register View Added

--- my_app/views/auth_view.py
+++ my_app/views/auth_view.py
@@ -1,4 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def register_view(request):
-    return render(request, 'auth/register_page.html')
+    errors = {}
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name', '').strip()
+        last_name = request.POST.get('last_name', '').strip()
+        
+        if not first_name:
+            errors['first_name'] = 'First name is required.'
+        if not last_name:
+            errors['last_name'] = 'Last name is required.'
+            
+        username = f"{first_name}{last_name}1".lower()
+        
+        if User.objects.filter(username=username).exists():
+            username = f"{first_name}{last_name}2".lower()
+            if User.objects.filter(username=username).exists():
+                username = f"{first_name}{last_name}3".lower()
+                if User.objects.filter(username=username).exists():
+                    username = f"{first_name}{last_name}4".lower()
+                    
+        password = 12345678
+            
+        if not errors:
+            user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name)
+            user.save()
+            messages.success(request, f'Account created successfully! Your username is {username} and your password is {password}. Please log in to continue.')
+            
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You have been logged in successfully!')
+                return redirect('home')
+            
+            messages.error(request, 'There was an error logging you in. Please try logging in manually.')
+            return redirect('login')
+        
+        return render(request, 'auth/register_page.html', {'errors': errors, 'data': request.POST})
+
+    return render(request, 'auth/register_page.html', {'data': request.POST})
