0be840ec0dc51e0c623e3eae7cbf88d10abaa79a
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-24 09:12:07 +0545

    Dynamic Score Fetched in Leaderboard Page

--- my_app/views/main.py
+++ my_app/views/main.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from ..models import Leaderboard
 
 def home_view(request):
     return render(request, 'main/home_page.html')
@@ -7,5 +8,7 @@ def games_view(request):
     return render(request, 'main/games_page.html')
 
 def leaderboard_view(request, game_name):
-    return render(request, 'main/leaderboard_page.html', {'game_name': game_name})
+    if game_name == 'Guess Game':
+        players = Leaderboard.objects.filter(game=game_name).order_by('score')
+    return render(request, 'main/leaderboard_page.html', {'game_name': game_name, 'players': players})
     
