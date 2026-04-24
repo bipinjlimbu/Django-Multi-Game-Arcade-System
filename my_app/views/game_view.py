b3d6231d83be214a2c8822bee52c6fbddcfa5651
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-24 09:52:48 +0545

    Reaction Game View Added

--- my_app/views/game_view.py
+++ my_app/views/game_view.py
@@ -23,4 +23,20 @@ def guess_view(request):
     return render(request, 'games/guess_game.html')
 
 def reaction_view(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            name = data.get('name')
+            score = data.get('score')
+            
+            leaderboard = Leaderboard.objects.create(user=request.user, score=score, game=name)
+            leaderboard.save()
+            
+            messages.success(request, 'Your score has been saved to the leaderboard!')
+            return redirect('reaction')
+            
+        except json.JSONDecodeError:
+            messages.error(request, 'Invalid JSON received')
+            return redirect('reaction')
+        
     return render(request, 'games/reaction_game.html')
