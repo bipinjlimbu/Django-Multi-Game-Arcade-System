9e2f4542552fa717f8749d37374fac3c903184d1
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-25 20:08:01 +0545

    Dynamically Game Fetching Done in Required Places

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
-from .views.main import home_view, games_view, leaderboard_selection_view, leaderboard_view
+from .views.main_view import home_view, games_view, leaderboard_selection_view, leaderboard_view
 from .views.game_view import guess_view, reaction_view
 
 urlpatterns = [
@@ -10,7 +10,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('games/', games_view, name='games'),
     path('leaderboard/', leaderboard_selection_view, name='leaderboard_selection'),
-    path('leaderboard/<str:game_name>/', leaderboard_view, name='leaderboard'),
+    path('leaderboard/<str:game_slug>/', leaderboard_view, name='leaderboard'),
     path('guess/', guess_view, name='guess'),
     path('reaction/', reaction_view, name='reaction'),
 ]
--- my_app/views/main_view.py
+++ my_app/views/main_view.py
@@ -11,12 +11,13 @@ def leaderboard_selection_view(request):
     games = Game.objects.all()
     return render(request, 'main/leaderboard_selection_page.html', {'games': games})
 
-def leaderboard_view(request, game_name):
-    if game_name == 'Guess Game':
-        players = Leaderboard.objects.filter(game=game_name).order_by('score')
+def leaderboard_view(request, game_slug):
+    game = Game.objects.get(slug=game_slug)
+    if game.name == 'Number Guess':
+        players = Leaderboard.objects.filter(game=game).order_by('score')
         
-    elif game_name == 'Reaction Game':
-        players = Leaderboard.objects.filter(game=game_name).order_by('-score')
+    elif game.name == 'Reaction Game':
+        players = Leaderboard.objects.filter(game=game).order_by('-score')
         
-    return render(request, 'main/leaderboard_page.html', {'game_name': game_name, 'players': players})
+    return render(request, 'main/leaderboard_page.html', {'game': game, 'players': players})
     
