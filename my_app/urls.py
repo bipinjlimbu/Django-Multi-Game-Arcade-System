33403bd1ebe07f825a6972e43a0f12e93993cb90
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-25 20:57:58 +0545

    Games View Made Simpler Removing Redundancy

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
 from .views.main_view import home_view, games_view, leaderboard_selection_view, leaderboard_view
-from .views.game_view import guess_view, reaction_view, memory_view
+from .views.game_view import game_view, save_score
 
 urlpatterns = [
     path('', home_view, name='home'),
@@ -11,7 +11,6 @@ urlpatterns = [
     path('games/', games_view, name='games'),
     path('leaderboard/', leaderboard_selection_view, name='leaderboard_selection'),
     path('leaderboard/<str:game_slug>/', leaderboard_view, name='leaderboard'),
-    path('games/number-guess/', guess_view, name='guess'),
-    path('games/reaction-game/', reaction_view, name='reaction'),
-    path('games/memory-game/', memory_view, name='memory'),
+    path('games/<str:game_slug>/', game_view, name='game_detail'),
+    path('save-score/', save_score, name='save_score'),
 ]
--- my_app/views/game_view.py
+++ my_app/views/game_view.py
@@ -3,7 +3,11 @@ from django.contrib import messages
 from ..models import Leaderboard, Game
 import json
 
-def guess_view(request):
+def game_view(request, game_slug):
+    game = Game.objects.get(slug=game_slug)
+    return render(request, f'games/{game_slug}.html', {'game': game})
+
+def save_score(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
@@ -17,45 +21,15 @@ def guess_view(request):
                 
             else:
                 leaderboard = Leaderboard.objects.get(user=request.user, game=game)
-                if score < leaderboard.score: 
+                if (game.name == 'Number Guess' and score < leaderboard.score) or (game.name == 'Reaction Game' and score > leaderboard.score):
                     leaderboard.score = score
                     leaderboard.save()
             
             messages.success(request, 'Your score has been saved to the leaderboard!')
-            return redirect('/games/number-guess/')
+            return redirect(f'/games/{slug}/')
             
         except json.JSONDecodeError:
             messages.error(request, 'Invalid JSON received')
-            return redirect('/games/number-guess/')
+            return redirect(f'/games/{slug}/')
 
-    return render(request, 'games/guess_game.html')
-
-def reaction_view(request):
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            slug = data.get('slug')
-            score = data.get('score')
-            game = Game.objects.get(slug=slug)
-
-            if not Leaderboard.objects.filter(user = request.user, game=game).exists():
-                leaderboard = Leaderboard.objects.create(user=request.user, score=score, game=game)
-                leaderboard.save()
-                
-            else:
-                leaderboard = Leaderboard.objects.get(user=request.user, game=game)
-                if score > leaderboard.score:
-                    leaderboard.score = score
-                    leaderboard.save()
-            
-            messages.success(request, 'Your score has been saved to the leaderboard!')
-            return redirect('/games/reaction-game/')
-            
-        except json.JSONDecodeError:
-            messages.error(request, 'Invalid JSON received')
-            return redirect('/games/reaction-game/')
-        
-    return render(request, 'games/reaction_game.html')
-
-def memory_view(request):
-    return render(request, 'games/memory_game.html')
+    return redirect(f'/games/{slug}/')
