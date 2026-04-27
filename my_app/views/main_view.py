580c107b6a539e9b2d2109a473af91d16ca5c7e2
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-27 20:12:43 +0545

    Arrow Defense Leaderboard Display Logic Added

--- my_app/views/main_view.py
+++ my_app/views/main_view.py
@@ -17,7 +17,7 @@ def leaderboard_view(request, game_slug):
     if game.name == 'Number Guess' or game.name == 'Memory Game':
         players = Leaderboard.objects.filter(game=game).order_by('score')
         
-    elif game.name == 'Reaction Game' or game.name == 'Math Challenge':
+    elif game.name == 'Reaction Game' or game.name == 'Math Challenge' or game.name == 'Arrow Defense':
         players = Leaderboard.objects.filter(game=game).order_by('-score')
         
     elif game.slug == 'quiz':
